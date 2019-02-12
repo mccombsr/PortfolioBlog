814f21774f01de1a1c41484ba65a7e2c34125b65
Ryan McCombs <mccombs008@yahoo.com>
2019-02-12 16:35:37 -0700

    started working on post thumbnail card"

--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -10,7 +10,15 @@ export default class PostList extends Component{
         return(
             <main className='postList'>
                 <p>This is the page content!!!</p>
-                
+                <div className="postThumbnail">
+                    <img src="https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg" alt="Post Image Thumbnail"/>
+                    <h1 className="postTitle">Dummy Post</h1>
+                    <p className="postBlurb">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ea quos asperiores commodi.</p>
+                    <aside>
+                        <h2 className="postDate">2/12/2019</h2>
+                        <h2 className="commentNum">88</h2>
+                    </aside>
+                </div>
                 <p>I'm the bottom!!!</p>
             </main>
         )
