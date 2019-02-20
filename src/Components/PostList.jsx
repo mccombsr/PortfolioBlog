d03fe2d20953a2e6d4b920c1ebef87cec3d88371
Ryan McCombs <mccombs008@yahoo.com>
2019-02-20 14:02:00 -0700

    finally have a half-way decent blog thumbnail

--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -11,13 +11,12 @@ export default class PostList extends Component{
             <main className='postList'>
                 {/* <p>This is the page content!!!</p> */}
                 <div className="postThumbnail">
-                    <img src="https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg" alt="Post Image Thumbnail"/>
+                    <div className="image"></div>
                     <h1 className="postTitle">Dummy Post</h1>
+                    {/* The blurb should just be the first 100 characters of the article, this way when the screen size changes it will just limit how much of the opening to the article you will see cutting off the excess with "..." */}
                     <p className="postBlurb">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ea quos asperiores commodi.</p>
-                    <aside>
                         <h2 className="postDate">2/12/2019</h2>
                         <h2 className="commentNum">88</h2>
-                    </aside>
                 </div>
                 {/* <p>I'm the bottom!!!</p> */}
             </main>
